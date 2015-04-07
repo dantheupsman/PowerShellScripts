53fb2a865463b28a970e20850405909b6dc46683
dantheupsman <dantheupsman@gmail.com>
2015-04-07 15:10:21 -0400

    Zip and User-Profile Initial Scripts

---
+++ User-Profile-Load.ps1
@@ -0,0 +1,18 @@
+﻿# users stored in csv with "username, password" format
+$userlist = 'c:\windows\Temp\Userlist.csv'
+
+
+#$Usernames = (import-csv $userlist).Username
+#$Passwords = (Import-csv $userlist).Password
+
+$Logins = Import-csv $userlist
+
+
+$Logins |
+ForEach-Object {
+    $spw = ConvertTo-SecureString $_.Password -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $_.Username,$spw
+    Start-Process cmd \c -WindowStyle hidden -Credential $cred -ErrorAction SilentlyContinue -LoadUserProfile
+}
+
+Remove-Item $userlist
---
+++ Zip-Directories.ps1
@@ -0,0 +1,12 @@
+﻿# Alias for 7-zip
+if (-not (test-path "$env:ProgramFiles\7-Zip\7z.exe")) {throw "$env:ProgramFiles\7-Zip\7z.exe needed"}
+set-alias sz "$env:ProgramFiles\7-Zip\7z.exe"
+ 
+$path = Read-Host 'What is the root that you would like to zip up?'
+ 
+foreach($dir in gci -Path $path | where{$_.PSIsContainer})
+{
+    $zipFile = $path + '\' + $dir.Name + ".7z"
+    sz a -m0=lzma2 -mx=9 -aoa -md=128m -mmt=4 "$zipfile" $dir.FullName
+    Remove-Item $dir.FullName -Recurse -Force
+}
